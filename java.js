65d46d3d06167bb9c89a154d84db9f5f9db113d0
csmatthew <46767726+csmatthew@users.noreply.github.com>
2024-10-22 10:37:33 +0100

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-   // Script to open and close sidebar
-   let btn = document.querySelector('#btn')
-   let sidebar = document.querySelector('.sidebar')
-
-   btn.onclick = function () {
-     sidebar.classList.toggle('active');
-   };
